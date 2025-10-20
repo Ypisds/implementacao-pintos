5433594310e216836341821db976606497bf9204
rpol <rpol@g3c34.cin.ufpe.br>
2025-10-20 18:49:26 -0300

    fix(syscall): Fix bug where argument acess uses wrong adress arithmetic

---
+++ userprog/syscall.c
@@ -0,0 +1,86 @@
+#include "userprog/syscall.h"
+#include <stdio.h>
+#include <syscall-nr.h>
+#include "threads/interrupt.h"
+#include "threads/thread.h"
+#include "userprog/process.h"
+
+static void syscall_handler (struct intr_frame *);
+
+void
+syscall_init (void) 
+{
+  intr_register_int (0x30, 3, INTR_ON, syscall_handler, "syscall");
+}
+
+// static void
+// syscall_handler (struct intr_frame *f) 
+// {
+//   int syscall_number = *(int *)f->esp;
+//   switch(syscall_number) {
+//     case SYS_HALT:
+//       shutdown_power_off();
+//       break;
+//     case SYS_EXIT:
+//       int status = *(int *)(f->esp+1);
+//       (f->eax) = status;
+//       process_exit();
+//       break;
+
+//     case SYS_WAIT:
+//       tid_t child_id = *(tid_t *)(f->esp+1);
+//       f->eax=child_id;
+//       process_wait(); 
+//       break;
+    
+//     case SYS_WRITE:
+//       int fd = *(int *)(f->esp+1);
+//       const void *buffer = *(void **)(f->esp+2);
+//       unsigned size = *(unsigned *)(f->esp+3);
+
+//       if(fd == 1) {
+//         putbuf((char*)buffer, size);
+//         f->eax = size;
+//       }
+
+//     break;
+
+//   }
+// }
+
+static void
+syscall_handler (struct intr_frame *f) 
+{
+  int syscall_number = *(int *)f->esp;
+
+  switch(syscall_number) {
+      case SYS_HALT:
+        shutdown_power_off();
+        break;
+      case SYS_EXIT:
+        int status = *((int *)f->esp+1);
+        (f->eax) = status;
+        printf ("%s: exit(%d)\n", thread_current()->name, status);
+        thread_exit();
+        break;
+      case SYS_WAIT:
+        tid_t child_id = *((tid_t *)f->esp+1);
+        f->eax=child_id;
+        process_wait(child_id); 
+        break;
+      case SYS_WRITE:
+        int fd = *((int *)f->esp+1);
+        const void *buffer = (const void *)*((int*)f->esp+2);
+        unsigned size = *((int *)f->esp+3);
+
+        if(fd == 1) {
+          putbuf((char*)buffer, size);
+          f->eax = size;
+        }
+        break;
+  }
+
+}
+
+
+
